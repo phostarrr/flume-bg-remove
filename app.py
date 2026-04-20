f644454d1f5d625bfedaa432207585b87809a138
Mpho Mabokela <mpho@flume.co.za>
2026-04-20 10:50:52 +0200

    api configuration

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+import requests 
+import os
+
 
 app = Flask(__name__)
+
+API_KEY = "2ycZNwEiuHKr9AxyfBUvhZDf"  # <-- PASTE YOUR ACTUAL API KEY HERE!
+UPLOAD_FOLDER = 'uploads'
+RESULT_FOLDER = 'results'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp'}
 
 @app.route('/')
 def hello_world():
